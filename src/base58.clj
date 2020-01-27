5299a67fd68803308d3e1af03d5bb62a9c306a9d
Martin Harrigan <martinharrigan@gmail.com>
2020-01-27 16:34:54 +0000

    Split encoding and decoding into multiple functions

--- src/base58.clj
+++ src/base58.clj
@@ -1,75 +1,105 @@
-(ns base58
-  (:require [clojure.string :as string]))
+(ns base58)
 
 (set! *warn-on-reflection* true)
 
-(def ^:const ^:private base58-alphabet
+(set! *unchecked-math* true)
+
+(def ^:const ^:private alphabet
   "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 
-(defn byte-array->base58-str
-  [ba]
-  (let [s  (alength ^bytes ba)
-        sb (StringBuilder.)
-        t  (int (Math/ceil (* s (/ (Math/log 256) (Math/log 58)))))
-        bb (byte-array t)]
-    (loop [i 0]
-      (when (< i s)
-        (if (zero? (bit-and (aget ^bytes ba i) 0xff))
-          (do
-            (.append sb (.charAt ^String base58-alphabet 0))
-            (recur (inc i)))
-          (loop [j i]
-            (when (< j s)
-              (loop [k 0 c (bit-and (aget ^bytes ba j) 0xff)]
-                (when (or (< k t) (pos? c))
-                  (let [d (bit-and (aget ^bytes bb k) 0xff)
-                        n (+ c (* 256 d))]
-                    (aset bb k (unchecked-byte (mod n 58)))
-                    (recur (inc k) (quot n 58)))))
-              (recur (inc j)))))))
-    (loop [i (dec t)]
-      (when (>= i 0)
-        (if (zero? (aget ^bytes bb i))
-          (recur (dec i))
-          (loop [j i]
-            (when (>= j 0)
-              (.append sb (.charAt ^String base58-alphabet
-                                   (bit-and (aget ^bytes bb j) 0xff)))
-              (recur (dec j)))))))
-    (.toString sb)))
+(def ^:const ^:private reverse-alphabet
+  (into {} (map-indexed #(vector %2 %1) alphabet)))
 
-(defn base58-str->byte-array
-  [b58s]
-  (let [s  (count b58s)
-        t  (int (Math/ceil (* s (/ (Math/log 58) (Math/log 256)))))
-        ba (byte-array t)
-        z  (loop [i 0]
-             (if (< i s)
-               (if (zero? (string/index-of base58-alphabet
-                                           (.charAt ^String b58s i)))
-                 (recur (inc i))
-                 (do
-                   (loop [j i]
-                     (when (< j s)
-                       (loop [k 0 c (string/index-of
-                                     base58-alphabet
-                                     (.charAt ^String b58s j))]
-                         (when (or (< k t) (pos? c))
-                           (let [d (bit-and (aget ^bytes ba k) 0xff)
-                                 n (+ c (* 58 d))]
-                             (aset ba k (unchecked-byte (mod n 256)))
-                             (recur (inc k) (quot n 256)))))
-                       (recur (inc j))))
-                   i))
-               i))]
-    (loop [i (dec t)]
-      (if (>= i 0)
-        (if (zero? (aget ^bytes ba i))
-          (recur (dec i))
-          (let [bb (byte-array (+ (inc i) z))]
-            (loop [j i]
-              (when (>= j 0)
-                (aset bb (+ (- i j) z) (aget ^bytes ba j))
-                (recur (dec j))))
-            bb))
-        (byte-array z)))))
+(defn- translate-encoding-array
+  [ba sz]
+  (loop [i (dec sz)]
+    (when-not (neg? i)
+      (if (zero? (aget ^bytes ba i))
+        (recur (dec i))
+        (loop [j i]
+          (when-not (neg? j)
+            (print (.charAt ^String alphabet
+                            (bit-and (aget ^bytes ba j) 0xff)))
+            (recur (dec j))))))))
+
+(defn estimate-str-size
+  "Estimates the size of a Base58 encoded string required to store a
+  byte array of size `sz`. The estimate is an upper bound."
+  [sz]
+  (inc (quot (* sz 137) 100)))
+
+(defn- construct-encoding-array
+  [ba sz zs]
+  (let [t  (estimate-str-size (- sz zs))
+        bb (byte-array t)]
+    (loop [i zs l 0]
+      (if (< i sz)
+        (recur (inc i)
+               (long (loop [k 0 c (bit-and (aget ^bytes ba i) 0xff)]
+                       (if (and (or (pos? c) (< k l)) (< k t))
+                         (let [d (bit-and (aget ^bytes bb k) 0xff)
+                               n (+ c (* 256 d))]
+                           (aset bb k (byte (rem n 58)))
+                           (recur (inc k) (quot n 58)))
+                         k))))
+        (translate-encoding-array bb l)))))
+
+(defn byte-array->str
+  [ba]
+  (with-out-str
+    (let [s (alength ^bytes ba)]
+      (loop [i 0]
+        (when (< i s)
+          (if (zero? (aget ^bytes ba i))
+            (do
+              (print (.charAt ^String alphabet 0))
+              (recur (inc i)))
+            (construct-encoding-array ba s i)))))))
+
+(defn- translate-decoding-array
+  [ba sz zs]
+  (loop [i (dec sz)]
+    (if (neg? i)
+      (byte-array zs)
+      (if (zero? (aget ^bytes ba i))
+        (recur (dec i))
+        (let [bb (byte-array (+ (inc i) zs))]
+          (loop [j i]
+            (if (neg? j)
+              bb
+              (do
+                (aset bb (+ (- i j) zs) (aget ^bytes ba j))
+                (recur (dec j))))))))))
+
+(defn estimate-byte-array-size
+  "Estimates the size of a byte array required to store a Base58
+  encoded string of size `sz`. The estimate is an upper bound."
+  [sz]
+  (inc (quot (* sz 74) 100)))
+
+(defn- construct-decoding-array
+  [s sz zs]
+  (let [t  (estimate-byte-array-size sz)
+        ba (byte-array t)]
+    (loop [j zs l 0]
+      (if (< j sz)
+        (recur (inc j)
+               (long (loop [k 0 c (reverse-alphabet
+                                   (.charAt ^String s j))]
+                       (if (and (or (pos? c) (< k l)) (< k t))
+                         (let [d (bit-and (aget ^bytes ba k) 0xff)
+                               n (+ c (* 58 d))]
+                           (aset ba k (byte (rem n 256)))
+                           (recur (inc k) (quot n 256)))
+                         k))))
+        (translate-decoding-array ba l zs)))))
+
+(defn str->byte-array
+  [s]
+  (let [t (count s)]
+    (loop [i 0]
+      (if (and (< i t) (= (.charAt ^String alphabet 0)
+                          (.charAt ^String s i)))
+        (recur (inc i))
+        (construct-decoding-array s t i)))))
+
--- test/base58_test.clj
+++ test/base58_test.clj
@@ -8,8 +8,23 @@
 
 (set! *warn-on-reflection* true)
 
-(defspec byte-array->base58-str-roundtrip-spec
+(defspec byte-array->str-roundtrip-spec
   (for-all [ba generators/bytes]
     (Arrays/equals ^bytes ba
-                   ^bytes (base58/base58-str->byte-array
-                           (base58/byte-array->base58-str ba)))))
+                   ^bytes (base58/str->byte-array
+                           (base58/byte-array->str ba)))))
+
+(defspec estimate-str-size-spec
+  (for-all [ba generators/bytes]
+    (let [s (alength ^bytes ba)]
+      (<= (count (base58/byte-array->str ba))
+          (int (Math/ceil (* s (/ (Math/log 256) (Math/log 58)))))
+          (base58/estimate-str-size s)))))
+
+(defspec estimate-byte-array-size-spec
+  (for-all [ba generators/bytes]
+    (let [b58s (base58/byte-array->str ba)
+          s (count b58s)]
+      (<= (alength ^bytes (base58/str->byte-array b58s))
+          (int (Math/ceil (* s (/ (Math/log 58) (Math/log 256)))))
+          (base58/estimate-byte-array-size s)))))
