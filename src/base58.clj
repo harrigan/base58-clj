4e5f1b9646a0de9e71c87d99ce361a0ac6b18613
Martin Harrigan <martinharrigan@gmail.com>
2020-01-28 12:34:50 +0000

    Test the intermediate functions

--- src/base58.clj
+++ src/base58.clj
@@ -10,25 +10,31 @@
 (def ^:const ^:private reverse-alphabet
   (into {} (map-indexed #(vector %2 %1) alphabet)))
 
-(defn- translate-encoding-array
+(defn reversed-indices->str
+  "Converts a byte array where each byte represents an index into the
+  Base58 alphabet, to a Base58 string by processing the array from
+  right to left. Leading zero indices on the right side of the array
+  are skipped. Only the first `sz` symbols on the left side of the
+  array are processed."
   [ba sz]
-  (loop [i (dec sz)]
-    (when-not (neg? i)
-      (if (zero? (aget ^bytes ba i))
-        (recur (dec i))
-        (loop [j i]
-          (when-not (neg? j)
-            (print (.charAt ^String alphabet
-                            (bit-and (aget ^bytes ba j) 0xff)))
-            (recur (dec j))))))))
+  (with-out-str
+    (loop [i (dec sz)]
+      (when-not (neg? i)
+        (if (zero? (aget ^bytes ba i))
+          (recur (dec i))
+          (loop [j i]
+            (when-not (neg? j)
+              (print (.charAt ^String alphabet
+                              (bit-and (aget ^bytes ba j) 0xff)))
+              (recur (dec j)))))))))
 
 (defn estimate-str-size
-  "Estimates the size of a Base58 encoded string required to store a
-  byte array of size `sz`. The estimate is an upper bound."
+  "Estimates the size of a Base58 string required to store a byte
+  array of size `sz`. The estimate is an upper bound."
   [sz]
   (inc (quot (* sz 137) 100)))
 
-(defn- construct-encoding-array
+(defn- byte-array->str*
   [ba sz zs]
   (let [t  (estimate-str-size (- sz zs))
         bb (byte-array t)]
@@ -42,9 +48,11 @@
                            (aset bb k (byte (rem n 58)))
                            (recur (inc k) (quot n 58)))
                          k))))
-        (translate-encoding-array bb l)))))
+        (reversed-indices->str bb l)))))
 
 (defn byte-array->str
+  "Converts a byte array to a Base58 string. Leading zeros are
+  preserved."
   [ba]
   (with-out-str
     (let [s (alength ^bytes ba)]
@@ -54,9 +62,14 @@
             (do
               (print (.charAt ^String alphabet 0))
               (recur (inc i)))
-            (construct-encoding-array ba s i)))))))
+            (print (byte-array->str* ba s i))))))))
 
-(defn- translate-decoding-array
+(defn reversed-indices->byte-array
+  "Converts a byte array where each byte represents an index into the
+  Base58 alphabet, to a byte array by processing the array from right
+  to left. Leading zero indices on the right side of the array are
+  skipped. Only the first `sz` symbols on the left side of the array
+  are processed. `zs` zeros are prepended to the output."
   [ba sz zs]
   (loop [i (dec sz)]
     (if (neg? i)
@@ -73,11 +86,11 @@
 
 (defn estimate-byte-array-size
   "Estimates the size of a byte array required to store a Base58
-  encoded string of size `sz`. The estimate is an upper bound."
+  string of size `sz`. The estimate is an upper bound."
   [sz]
   (inc (quot (* sz 74) 100)))
 
-(defn- construct-decoding-array
+(defn- str->byte-array*
   [s sz zs]
   (let [t  (estimate-byte-array-size sz)
         ba (byte-array t)]
@@ -92,14 +105,15 @@
                            (aset ba k (byte (rem n 256)))
                            (recur (inc k) (quot n 256)))
                          k))))
-        (translate-decoding-array ba l zs)))))
+        (reversed-indices->byte-array ba l zs)))))
 
 (defn str->byte-array
+  "Converts a Base58 string to a byte array. Leading zeros are
+  preserved."
   [s]
   (let [t (count s)]
     (loop [i 0]
       (if (and (< i t) (= (.charAt ^String alphabet 0)
                           (.charAt ^String s i)))
         (recur (inc i))
-        (construct-decoding-array s t i)))))
-
+        (str->byte-array* s t i)))))
--- test/base58_test.clj
+++ test/base58_test.clj
@@ -8,6 +8,17 @@
 
 (set! *warn-on-reflection* true)
 
+(deftest reversed-indices->str-test
+  (are [x y z]
+      (= x (base58/reversed-indices->str (byte-array y) z))
+    ""    []       0
+    ""    [0]      1
+    "2"   [1 0]    2
+    "2z"  [57 1 0] 3
+    "21z" [57 0 1] 3
+    "2"   [1 0]    1
+    ""    [0 1]    1))
+
 (defspec byte-array->str-roundtrip-spec
   (for-all [ba generators/bytes]
     (Arrays/equals ^bytes ba
@@ -21,10 +32,24 @@
           (int (Math/ceil (* s (/ (Math/log 256) (Math/log 58)))))
           (base58/estimate-str-size s)))))
 
+(deftest reversed-indices->byte-array-test
+  (are [w x y z]
+      (let [ba (base58/reversed-indices->byte-array (byte-array x)
+                                                    y z)]
+        (Arrays/equals (byte-array w) ^bytes ba))
+    []               []       0 0
+    []               [0]      1 0
+    [0x01]           [1 0]    2 0
+    [0x01 0x39]      [57 1 0] 3 0
+    [0x01 0x00 0x39] [57 0 1] 3 0
+    [0x01]           [1 0]    1 0
+    []               [0 1]    1 0
+    [0x00 0x01]      [1]      1 1))
+
 (defspec estimate-byte-array-size-spec
   (for-all [ba generators/bytes]
     (let [b58s (base58/byte-array->str ba)
-          s (count b58s)]
+          s    (count b58s)]
       (<= (alength ^bytes (base58/str->byte-array b58s))
           (int (Math/ceil (* s (/ (Math/log 58) (Math/log 256)))))
           (base58/estimate-byte-array-size s)))))
